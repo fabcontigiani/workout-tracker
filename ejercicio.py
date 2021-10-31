965902e60fadf22e0d3b6e4be4c3df74c7bae0ce
Fabrizio Contigiani <fabcontigiani@gmail.com>
2021-10-31 12:26:58 -0300

    Add getters and some method implementation

--- ejercicio.py
+++ ejercicio.py
@@ -1,29 +1,75 @@
-class Ejercicio:
+from abc import ABC, abstractmethod
+
+class Ejercicio(ABC):
     def __init__(self, nombre: str, grupoMuscular: str):
         self.__nombre = nombre
         self.__grupoMuscular = grupoMuscular
+        self.__esCardio = None
+
+    @property
+    def nombre(self):
+        return self.__nombre
+
+    @property
+    def grupoMuscular(self):
+        return self.__grupoMuscular
+
+    @property
+    def esCardio(self):
+        return self.__esCardio
+
+    @abstractmethod
+    def volumen(self):
+        pass
 
 
 class PesoReps(Ejercicio):
-    def __init__(self, nombre: str, grupoMuscular: str, esCardio: bool, peso: int, reps: int):
+    def __init__(self, nombre: str, grupoMuscular: str, esCardio: bool, peso: int, reps: int, sets: int):
         super().__init__(nombre, grupoMuscular)
         self.__esCardio = False
         self.__peso = peso
         self.__reps = reps
-        self.__volumen = peso * reps
+        self.__sets = sets
+
+    @property
+    def peso(self):
+        return self.__peso
+
+    @property
+    def reps(self):
+        return self.__reps
+
+    @property
+    def sets(self):
+        return self.__sets
 
 
 class Reps(Ejercicio):
-    def __init__(self, nombre: str, grupoMuscular: str, esCardio: bool, pesoCorporal: float, reps: int):
+    def __init__(self, nombre: str, grupoMuscular: str, esCardio: bool, pesoCorporal: float, reps: int, sets: int):
         super().__init__(nombre, grupoMuscular)
         self.__esCardio = False
         self.__pesoCorporal = pesoCorporal
         self.__reps = reps
-        self.__volumen = pesoCorporal * reps
+        self.__sets = sets
 
+    @property
+    def pesoCorporal(self):
+        return self.__pesoCorporal
+
+    @property
+    def reps(self):
+        return self.__reps
+
+    @property
+    def sets(self):
+        return self.__sets
 
 class Tiempo(Ejercicio):
     def __init__(self, nombre: str, grupoMuscular: str, tiempo: int):
-        self.__esCardio = True
         super().__init__(nombre, grupoMuscular)
+        self.__esCardio = True
         self.__tiempo = tiempo
+
+    @property
+    def tiempo(self):
+        return self.__tiempo
--- usuario.py
+++ usuario.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 class Usuario:
     def __init__(self, nombre: str, contrasena: str, peso: float):
         self.__nombre = nombre
@@ -23,3 +25,27 @@ class Usuario:
 
     def toggleIngreso(self):
         self.__haIngresado = not self.__haIngresado
+
+    def agregarEjercicio(self):
+        print("[1] Peso+Reps\n[2] Reps\n[3] Tiempo")
+        tipo = input("Tipo de ejercicio >> ")
+        if tipo == 1:
+            peso = input("Peso (en kg):")
+            while(not isinstance(peso, float)):
+                print("Por favor, ingrese un valor valido.\n")
+                peso = input("Peso (en kg):")
+            reps = input("Nro de repeticiones: ")
+            while (not isinstance(reps, int)):
+                print("Por favor, ingrese un valor valido.\n")
+                reps = input("Nro de repeticiones: ")
+            sets = input("Nro de series: ")
+            while (not isinstance(sets, int)):
+                print("Por favor, ingrese un valor valido.\n")
+                sets = input("Nro de sets: ")
+        elif tipo == 2:
+            pass
+        elif tipo == 3:
+            pass
+        else:
+            print("Tipo de ejercicio invalido.")
+            return
